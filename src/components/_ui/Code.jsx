148b5a418b29f71dea77008b2a3f5fd5ba33251b
Cesar de Carvalho Boaventura <cesar.boaventura61@gmail.com>
2018-09-26 20:30:50 -0300

    code component 100%

--- src/components/_ui/Code.jsx
+++ src/components/_ui/Code.jsx
@@ -3,11 +3,13 @@ import styled from 'styled-components';
 const Code = styled.code`
   background-color: #3e606f;
   border: 1px solid #999;
-  display: block;
+  display: inline-block;
+  box-sizing: border-box;
   font-size: 16px;
   overflow-x: scroll;
   padding: 20px;
   text-align: center;
+  width: 100%;
 `;
 
 export default Code;
