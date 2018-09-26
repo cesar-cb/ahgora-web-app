48f2c4f0d48dde5839fb93e509f0f47b5a3bdd6a
Cesar de Carvalho Boaventura <cesar.boaventura61@gmail.com>
2018-09-26 17:21:48 -0300

    style variables

---
+++ src/components/settings.js
@@ -0,0 +1,10 @@
+export const $baseFonts = "'SF Mono', monospace, Neue, sans-serif";
+export const $headingFont = $baseFonts;
+
+export const $primaryColor = '#aedcf0';
+export const $secondaryColor = '#242F3F';
+export const $terciaryColor = '#91aa9d';
+export const $bgColor = $secondaryColor;
+
+export const $black = 'black';
+export const $white = 'white';
