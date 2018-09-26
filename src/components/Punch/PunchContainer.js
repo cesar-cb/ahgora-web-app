acd8342d5101827c84f60c48939a49b068481b2c
Cesar de Carvalho Boaventura <cesar.boaventura61@gmail.com>
2018-09-26 20:26:24 -0300

    punch container

--- src/components/Punch/PunchContainer.js
+++ src/components/Punch/PunchContainer.js
@@ -1,7 +1,45 @@
 import { Container } from 'unstated';
 
 class PunchContainer extends Container {
+  state = {
+    response: null,
+    loading: false,
+    error: null
+  };
 
+  _fetch = async ({ account, password, id }) => {
+    try {
+      this.setState({ loading: true });
+
+      const conf = {
+        method: 'POST',
+        body: {
+          account,
+          password,
+          identity: id,
+          key: '',
+          origin: 'pw2'
+        }
+      };
+
+      const response = await fetch('http://www.ahgora.com.br/batidaonline/verifyIdentification', conf);
+      const json = await response.json();
+
+      return json;
+    } catch (error) {
+      this.setState({ error });
+    } finally {
+      this.setState({ loading: false });
+    }
+  };
+
+  do = async ({ account, password, id }) => {
+    const response = await this._fetch({ account, password, id });
+
+    this.setState({
+      response
+    });
+  };
 }
 
 export default PunchContainer;
