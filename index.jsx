c2bbd2a36d6233fc34513ff928c448d9071a49e2
Cesar de Carvalho Boaventura <cesar.boaventura61@gmail.com>
2018-09-26 20:24:56 -0300

    reset page margin and padding

--- index.jsx
+++ index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'unstated'
-import { ìnjectGlobal, injectGlobal } from 'styled-components'
+import { Provider } from 'unstated';
+import { ìnjectGlobal, injectGlobal } from 'styled-components';
 
 import App from '@/components/App';
 
@@ -9,8 +9,10 @@ injectGlobal`
   html,
   body {
     font-family: 'SF Mono', monospace;
+    padding: 0;
+    margin: 0;
   }
-`
+`;
 
 ReactDOM.render(
   <Provider>
