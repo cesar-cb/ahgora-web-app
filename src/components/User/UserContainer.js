a9c8918c127f5af1fc64a08a140e83e27d75c3f8
Cesar de Carvalho Boaventura <cesar.boaventura61@gmail.com>
2018-09-26 20:26:34 -0300

    user container

--- src/components/User/UserContainer.js
+++ src/components/User/UserContainer.js
@@ -1,12 +1,13 @@
 import { Container } from 'unstated';
 
-
 class UserContainer extends Container {
   state = {
     information: null,
-  }
+    error: false
+  };
 
   initInformation = () => {
+    const name = localStorage.getItem('name');
     const account = localStorage.getItem('account');
     const password = localStorage.getItem('password');
     const id = localStorage.getItem('id');
@@ -14,20 +15,34 @@ class UserContainer extends Container {
     if (account && password && id) {
       this.setState({
         information: {
+          name,
           account,
           password,
-          id,
+          id
         }
       });
     }
-  }
+  };
 
-  setInformation = ({ account, password, id }) =>
-    this.setState({
-      account,
-      password,
-      id,
-    });
+  setInformation = ({ name, account, password, id }) => {
+    try {
+      localStorage.setItem('name', name);
+      localStorage.setItem('account', account);
+      localStorage.setItem('password', password);
+      localStorage.setItem('id', id);
+
+      this.setState({
+        information: {
+          name,
+          account,
+          password,
+          id
+        }
+      });
+    } catch (error) {
+      this.setState({ error: error });
+    }
+  };
 }
 
 export default UserContainer;
