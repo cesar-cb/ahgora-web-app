707cc054522f166b836c193e3ec6873537c7b5fd
Cesar de Carvalho Boaventura <cesar.boaventura61@gmail.com>
2018-09-26 20:27:45 -0300

    Punch page

--- src/components/Punch/Punch.jsx
+++ src/components/Punch/Punch.jsx
@@ -1,5 +1,37 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Subscribe } from 'unstated';
+import UserContainer from '@/components/User/UserContainer';
+import PunchContainer from '@/components/Punch/PunchContainer';
 
-const Punch = () => <div>Punch Meeee</div>;
+import Response from '@/components/Punch/Response';
+
+import Button from '@/components/_ui/Button';
+import Loader from '@/components/_ui/Loader';
+import Container from '@/components/_ui/Container';
+import Spacer from '@/components/_ui/Spacer';
+
+const Punch = () => (
+  <Subscribe to={[PunchContainer, UserContainer]}>
+    {(punch, user) => {
+      const { account, password, id } = user.state.information;
+
+      return (
+        <Container>
+          {punch.state.loading && <Loader />}
+          <h1 style={{ textAlign: 'center', width: '100%' }}>
+            Olá
+            {' '}
+            {user.state.information.name}
+          </h1>
+          <Button big onClick={() => punch.do({ account, password, id })}>
+            Bater Ponto!
+          </Button>
+          <Spacer margin={[20, 0, 0, 0]} />
+          {punch.state.response && <Response>{punch.state.response}</Response>}
+        </Container>
+      );
+    }}
+  </Subscribe>
+);
 
 export default Punch;
