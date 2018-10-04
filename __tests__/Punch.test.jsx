3a7304dd364a16bdb78e9eb6b34e1c4138728026
Cesar de Carvalho Boaventura <cesar.boaventura61@gmail.com>
2018-10-03 22:34:21 -0300

    fix props test

--- __tests__/Punch.test.jsx
+++ __tests__/Punch.test.jsx
@@ -7,86 +7,54 @@ import Punch from '@/components/Punch/Punch';
 import PunchContainer from '@/components/Punch/PunchContainer';
 import UserContainer from '@/components/User/UserContainer';
 
+import Response from '@/components/Punch/Response';
+
 const defer = (fn, done) => {
   return tck(() => {
     fn();
     done();
   });
-}
+};
 
 describe('Punch', () => {
-  describe('Container', () => {
-    test('should have correct initial state', () => {
-      const punch = new PunchContainer();
+  test('should have correct initial state', () => {
+    const punch = new PunchContainer();
 
-      expect(punch.state.response).toEqual(null);
-      expect(punch.state.loading).toEqual(false);
-      expect(punch.state.error).toEqual(null);
-    });
-    test('should change loading state', async (done) => {
-      const punch = new PunchContainer();
-      const user = new UserContainer();
+    expect(punch.state.response).toEqual(null);
+    expect(punch.state.loading).toEqual(false);
+    expect(punch.state.error).toEqual(null);
+  });
+  test('should pass correct response props', async done => {
+    const punch = new PunchContainer();
+    const user = new UserContainer();
 
-      await user.setState({
-        information: {
-          name: 'test user',
-          account: '456',
-          password: '159',
-          id: '1231k24gb'
-        }
-      });
+    const response = {
+      error: false,
+      message: 'generic message',
+      response: 'generic response'
+    };
 
-      const tree = mount(
-        <Provider inject={[punch, user]}>
-          <Punch />
-        </Provider>
-      );
-
-      tree.find('#punch').hostNodes().simulate('click');
-
-      defer(() => expect(punch.state.loading).toBeTruthy(), done);
-
-    });
-    test('should pass correct response', async (done) => {
-      const punch = new PunchContainer();
-      const user = new UserContainer();
-
-      const response = {
-        error: false,
-        message: 'generic message',
-        response: 'generic response',
-      };
-
-      await user.setState({
-        information: {
-          name: 'test user',
-          account: '456',
-          password: '159',
-          id: '1231k24gb',
-        },
-      });
-
-      tree.find('#punch').hostNodes().simulate('click');
-
-      await punch.setState({
-        response: {
-          error: false,
-          message: 'generic message',
-          response: 'generic response',
-        }
-      })
-
-      const tree = mount(
-        <Provider inject={[punch, user]}>
-          <Punch />
-        </Provider>
-      );
-
-      const responseProps = tree.find('#response').hostNodes().simulate('click')
-
-      defer(() => expect(responseProps).toEqual(response), done);
-
+    await user.setState({
+      information: {
+        name: 'test user',
+        account: '456',
+        password: '159',
+        id: '1231k24gb'
+      }
     });
 
+    await punch.setState({
+      response
+    });
+
+    const tree = mount(
+      <Provider inject={[punch, user]}>
+        <Punch />
+      </Provider>
+    );
+
+    const responseProps = tree.find(Response).prop('response');
+
+    defer(() => expect(responseProps).toEqual(response), done);
   });
 });
