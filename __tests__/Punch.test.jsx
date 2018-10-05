58d5b75e18c59512054fd19a1eb0d44a21405bb3
Cesar Boaventura <cesar.boaventura61@gmail.com>
2018-10-05 13:42:04 -0300

    added punch tests

--- __tests__/Punch.test.jsx
+++ __tests__/Punch.test.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { shallow, render, mount } from 'enzyme';
-import { Provider, Subscribe } from 'unstated';
+import { mount } from 'enzyme';
+import { Provider } from 'unstated';
 import tck from 'tickedoff';
+import { stub } from 'sinon'
 
 import Punch from '@/components/Punch/Punch';
 import PunchContainer from '@/components/Punch/PunchContainer';
@@ -16,45 +17,66 @@ const defer = (fn, done) => {
   });
 };
 
+let punch;
+let user;
+
+let tree;
+
+const accountInformation = {
+  account: '456',
+  password: '159',
+  id: '1231k24gb'
+};
+
+const fullInformation = Object.assign({}, accountInformation, {
+  name: 'test user'
+});
+
+const response = {
+  error: false,
+  message: 'generic message',
+  response: 'generic response'
+};
+
+beforeEach(async () => {
+  punch = new PunchContainer();
+  user = new UserContainer();
+
+  await user.setState({
+    information: fullInformation,
+  });
+
+  await punch.setState({
+    response,
+  });
+
+  tree = mount(
+    <Provider inject={[punch, user]}>
+      <Punch />
+    </Provider>
+  );
+});
+
 describe('Punch', () => {
-  test('should have correct initial state', () => {
+  test('should has correct initial state', () => {
     const punch = new PunchContainer();
 
     expect(punch.state.response).toEqual(null);
     expect(punch.state.loading).toEqual(false);
     expect(punch.state.error).toEqual(null);
   });
-  test('should pass correct response props', async done => {
-    const punch = new PunchContainer();
-    const user = new UserContainer();
 
-    const response = {
-      error: false,
-      message: 'generic message',
-      response: 'generic response'
-    };
-
-    await user.setState({
-      information: {
-        name: 'test user',
-        account: '456',
-        password: '159',
-        id: '1231k24gb'
-      }
-    });
-
-    await punch.setState({
-      response
-    });
-
-    const tree = mount(
-      <Provider inject={[punch, user]}>
-        <Punch />
-      </Provider>
-    );
-
+  test('should pass correct response props', done => {
     const responseProps = tree.find(Response).prop('response');
 
     defer(() => expect(responseProps).toEqual(response), done);
+  });
+
+  test('should call do with correct params', () => {
+    const spy = stub(punch, 'do');
+
+    tree.find('button#punch').simulate('click');
+
+    expect(spy.calledOnceWith(accountInformation)).toBeTruthy();
   });
 });
