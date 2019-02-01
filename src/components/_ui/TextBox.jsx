93557bc4987904cb80854c64c870083c9e3d22df
Cesar Boaventura <cesar.boaventura61@gmail.com>
2019-01-31 22:48:42 -0200

    Squashed commit of the following:
    commit fef5df0d0f43d894a0d67f72339f0e82152452e0
    Author: Cesar Boaventura <cesar.boaventura61@gmail.com>
    Date:   Thu Jan 31 22:41:34 2019 -0200
    
        added missing TextBox label propTypes
    
    commit a46131487d839dbf6d5e9d9e8e0c2b2018735dec
    Author: Cesar Boaventura <cesar.boaventura61@gmail.com>
    Date:   Thu Jan 31 22:38:59 2019 -0200
    
        update yarn.lock
    
    commit 90a552d11e9212247584e6a0a85ed7b641ee4074
    Author: Cesar Boaventura <cesar.boaventura61@gmail.com>
    Date:   Thu Jan 31 22:31:57 2019 -0200
    
        pretty print api response

--- src/components/_ui/TextBox.jsx
+++ src/components/_ui/TextBox.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import Label from '@/components/_ui/Label';
 import Input from '@/components/_ui/Input';
 
-const TextBox = ({label, ...props}) => (
+const TextBox = ({ label, ...props }) => (
   <Label style={{ marginBottom: '20px' }}>
     {label}
     <Input {...props} />
   </Label>
-)
+);
+
+TextBox.propTypes = {
+  label: PropTypes.string
+};
 
 export default TextBox;
