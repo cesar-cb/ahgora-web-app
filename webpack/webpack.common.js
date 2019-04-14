7dcc23bad474fef0d355a9496bc93790a4210469
Cesar Boaventura <cesar.boaventura61@gmail.com>
2019-04-14 17:04:58 -0300

    Squashed commit of the following:
    commit 91666a229fef6c8932f964aa3cc73cc1da843d04
    Author: Cesar Boaventura <cesar.boaventura61@gmail.com>
    Date:   Sun Apr 14 17:02:59 2019 -0300
    
        feat(favicon): changed favicon and added webapp-webpack-plugin

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const WebappWebpackPlugin = require('webapp-webpack-plugin')
 
 const path = require('path');
 
@@ -24,10 +25,21 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
-      filename: 'index.html'
+      filename: 'index.html',
     }),
     new CleanWebpackPlugin('dist', {
-      root: path.resolve(__dirname, '../')
-    })
-  ]
+      root: path.resolve(__dirname, '../'),
+    }),
+    new WebappWebpackPlugin({
+      logo: path.resolve(__dirname, '../src/favicon.png'),
+      favicons: {
+        appName: 'ahgora-web-app',
+        appDescription: 'Ahgora punch pointer wrapper',
+        developerName: 'Cesar Boaventura',
+        developerURL: 'https://cesarboaventura.dev',
+        background: '#242F3F',
+        theme_color: '#242F3F',
+      }
+    }),
+  ],
 };
